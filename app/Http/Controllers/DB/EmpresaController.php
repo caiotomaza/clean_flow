c917d125722f81babf4d8856bdaba2c0a0cb1284
JulioSAlencar <jsalencar24@gmail.com>
2025-04-08 13:55:21 -0300

    chore: move database related controllers
    - ajustar namespace
    - adicionar referência de controlador

--- app/Http/Controllers/DB/EmpresaController.php
+++ app/Http/Controllers/DB/EmpresaController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\DB;
 
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class EmpresaController extends Controller
