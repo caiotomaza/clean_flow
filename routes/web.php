dcc6bcfe7bf56edbd69268df5d51e2411008c64f
Caio Tomaz <caiotomaza@gmail.com>
2025-04-15 21:11:38 -0300

    style - organização das pasta e rotas

--- routes/web.php
+++ routes/web.php
@@ -1,39 +1,43 @@
 <?php
 
-use App\Http\Controllers\entrada;
-use App\Http\Controllers\Page\EntradaController;
-use App\Http\Controllers\page\EntradaMaterialController;
-use App\Http\Controllers\Page\HistoricoController;
-use App\Http\Controllers\page\RegisterUserController;
-use App\Http\Controllers\Page\RelatorioController;
-use App\Http\Controllers\Page\UsuariosController;
-use App\Http\Controllers\ProfileController;
-use Illuminate\Support\Facades\Route;
+    require __DIR__.'/auth.php';
 
-Route::get('/', function () {
-    return view('welcome');
-});
+    use App\Http\Controllers\entrada;
+    use App\Http\Controllers\Page\EntradaController;
+    use App\Http\Controllers\page\EntradaMaterialController;
+    use App\Http\Controllers\Page\HistoricoController;
+    use App\Http\Controllers\page\RegisterUserController;
+    use App\Http\Controllers\Page\RelatorioController;
+    use App\Http\Controllers\Page\UsuariosController;
+    use App\Http\Controllers\ProfileController;
+    use Illuminate\Support\Facades\Route;
+    use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+    Route::get('/', function () {
+        return view('welcome');
+    });
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/entrada',[EntradaMaterialController::class, 'page'])->name('entrada.index');
-Route::get('/historico',[HistoricoController::class, 'page'])->name('historico.index');
-Route::get('/relatorios',[RelatorioController::class, 'page'])->name('relatorios.index');
+    /*
+    Route::middleware('auth')->group(function () {
+        Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+        Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+        Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    });
+    */
 
-// Usuarios
-Route::get('/usuarios',[UsuariosController::class, 'page'])->name('usuarios.index');
-Route::get('/usuarios/create', [UsuariosController::class, 'create'])->name('usuarios.create');
-Route::post('/usuarios', [UsuariosController::class, 'store'])->name('usuarios.store');
-Route::put('/usuarios/{id}', [UsuariosController::class, 'update'])->name('usuarios.update');
+    // Navegação
+    Route::get('/dashboard', function () {return view('dashboard.index');});
+    Route::get('/historicos', function () {return view('historicos.index');});
+    Route::get('/relatorios', function () {return view('relatorios.index');});
+    Route::get('/usuarios', function () {return view('usuarios.index');});
 
+    // Usuario
+    Route::get('/usuarios/create', [UsuariosController::class, 'create'])->name('usuarios.create');
+    Route::post('/usuarios', [UsuariosController::class, 'store'])->name('usuarios.store');
+    Route::put('/usuarios/{id}', [UsuariosController::class, 'update'])->name('usuarios.update');
 
-
-require __DIR__.'/auth.php';
+?>
