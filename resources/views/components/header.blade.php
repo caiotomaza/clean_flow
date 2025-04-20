124bdffff6c7f5688b45ccc011dcd59d87872e0b
ArthurFontenele <jsalencar24@gmail.com>
2025-04-19 21:07:28 -0300

    feat(layout): Resolvendo diferenças de css

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -4,7 +4,6 @@
     ?>
     <head>
         <link rel="stylesheet" href="{{ asset('css/header/style.css') }}">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     </head>
     <header class="custom-header">
         <div class="custom-menu">
