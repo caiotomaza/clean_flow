18d8505e33e1cf0238e8720b154ccea6b17b8eab
Caio Tomaz <caiotomaza@gmail.com>
2025-05-20 19:43:32 -0300

    fix - delete de arquivos bagunçados

--- database/factories/ResiduosSaiFactory.php
+++ database/factories/ResiduosSaiFactory.php
@@ -5,7 +5,7 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\reseduosSai>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\ResiduosSai>
  */
 class ResiduosSaiFactory extends Factory
 {
